c201685c01ef0d2bf4a0ea57bc1bfc7e6bebf455
Arvind Vedula <avedula@outlook.com>
2026-04-03 23:16:45 -0400

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        // remove the spaces
+        let str = s.split(' ').join('').replace(/[^a-z0-9]/gi, "").toLowerCase();
+        let i = 0;
+        let j = str.length - 1;
+        // start checking from both the ends if the chars are similar
+        
+        while(i < j){
+            console.log('i', i, str[i]);
+            console.log('j', j, str[j]);
+            
+            if(str[i] !== str[j]){
+                return false
+            }
+            i++;
+            j--;
+
+        }
+
+        return true;
+    }
+}
