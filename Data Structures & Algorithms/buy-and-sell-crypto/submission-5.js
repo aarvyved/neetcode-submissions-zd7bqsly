7437349945bda0ca23d900548b54d8ce0aa3df05
Arvind Vedula <avedula@outlook.com>
2026-04-08 22:15:14 -0400

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let l = 0;
+        let r = 1;
+        let maxP = 0;
+        while(r < prices.length){
+            if(prices[r] > prices[l]){
+                let profit = prices[r] - prices[l];
+                maxP = Math.max(profit, maxP);
+            } else {
+                l = r;
+            }
+            r++;
+        }
+        return maxP;
+    }
+}
