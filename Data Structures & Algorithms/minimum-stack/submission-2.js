27cc5b1b27b3ccc9869084fe9bbb711ef2c9b3d0
Arvind Vedula <avedula@outlook.com>
2026-04-16 14:15:53 -0400

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.js
@@ -0,0 +1,55 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        // this.min = Infinity;
+        this.minArr = []
+        this.length = 0;
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+        this.length++;
+        // set min array element
+        if(this.minArr.length > 0){
+            let min = Math.min(this.minArr[this.minArr.length - 1], val);
+            this.minArr.push(min);
+        } else {
+            this.minArr.push(val);
+        }
+
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        let topEl = this.stack[this.length - 1];
+        this.stack.pop();
+        // update minArr
+        // if(this.minArr[this.minArr.length - 1] === topEl){
+        //     this.minArr.pop();
+        // }
+        this.length--;
+        this.minArr.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        let topEl = this.stack[this.length - 1];
+        return this.length ? this.stack[this.length-1] : "";
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        let minLength = this.minArr.length;
+        return this.minArr[minLength - 1];
+    }
+}
