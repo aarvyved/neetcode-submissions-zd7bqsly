9dddcaf219e7e64408c4d50a324aad9095e3a104
Arvind Vedula <avedula@outlook.com>
2026-04-20 20:07:50 -0400

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let braceMap = {
+            "(": ")",
+            "{": "}",
+            "[": "]",
+        }
+        let bracesArr = s.split("");
+        let stack = [];
+        for(let i = 0; i < bracesArr.length; i++){
+            let stackTop = stack[stack.length - 1];
+            if(braceMap[stackTop] === bracesArr[i]){
+                stack.pop();
+            } else {
+                stack.push(bracesArr[i]);
+            }
+        }
+        return !(stack.length > 0)
+    }
+    
+}
