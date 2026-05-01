7b8200a973ff7211faa0cb110565e7c1ba62809b
Arvind Vedula <avedula@outlook.com>
2026-05-01 16:33:04 -0400

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+        let visited = new Set();
+        console.log(head);
+        let currNode = head;
+        while(currNode != null){
+            console.log(currNode);
+            if(visited.has(currNode)){
+                return true;
+            }
+            visited.add(currNode);
+            currNode = currNode.next;
+            
+        }
+        return false;
+
+    }
+}
