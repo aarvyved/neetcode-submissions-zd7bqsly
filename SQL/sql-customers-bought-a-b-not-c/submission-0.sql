772e923ef18950027c7512ac6dc137994c53dab0
Arvind Vedula <avedula@outlook.com>
2026-05-13 16:03:05 -0400

    Add: sql-customers-bought-a-b-not-c - submission-0

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-0.sql
@@ -0,0 +1,16 @@
+-- Write your query below
+SELECT customer_id, customer_name
+FROM customers
+WHERE customer_id IN (
+    -- customers bought A
+    SELECT customer_id from orders WHERE product_name = 'A'
+)
+AND customer_id IN (
+    -- customers bought B
+    SELECT customer_id from orders WHERE product_name = 'B'
+)
+AND customer_id NOT IN (
+    -- customers bought B
+    SELECT customer_id from orders WHERE product_name = 'C'
+)
+ORDER BY customer_name;
