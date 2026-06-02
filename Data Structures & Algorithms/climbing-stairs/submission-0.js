382fd50234104c7e20624b1ffaee72d9caaa5d61
Arvind Vedula <avedula@outlook.com>
2026-06-01 21:53:03 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    hash = {};
+    climbStairs(n) {
+        if(n <= 2){
+            return n;
+        }
+        
+        if(!this.hash[n]){
+            
+            this.hash[n] = this.climbStairs(n-1) + this.climbStairs(n-2);
+            
+        }
+        return this.hash[n];
+
+    
+    }
+}
