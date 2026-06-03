27b71ad4491e1c3e5c89d7b38f93dcded5d286ef
Arvind Vedula <avedula@outlook.com>
2026-06-02 20:25:25 -0400

    Add: insertionSort - submission-0

---
+++ Data Structures & Algorithms/insertionSort/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {Pair[]}
+     * @returns {Pair[][]}
+     */
+    insertionSort(pairs) {
+        let stateArr = [];
+        for(let i = 0; i < pairs.length; i++){
+            let j = i - 1;
+            
+
+            while(j >= 0 && pairs[j + 1].key < pairs[j].key){
+                let tmp = pairs[j + 1];
+                pairs[j + 1] = pairs[j];
+                pairs[j] = tmp;
+                j --;
+            }
+
+
+            
+
+            stateArr.push([...pairs]);
+        }
+        console.log('pairs', pairs);
+        return stateArr;
+    }
+}
